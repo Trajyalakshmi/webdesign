3b56ba37408c9c28cba756dfa802e16008eef76e
Trajyalakshmi <raajivvit@gmail.com>
2020-05-23 12:48:13 +0530

    Raaji

---
+++ js/index.js
@@ -0,0 +1,80 @@
+fetch('./data.json').then(object=>
+	
+
+	)
+
+
+
+
+	// var body=document.getElementById("root");
+	
+	// var h1=document.createElement("h1");
+	// h1.textContent="Resume";
+	// body.appendChild(h1);
+
+
+	// var main=document.createElement("section");
+	// main.classList.add("main");
+	// body.appendChild(main);
+
+	// var leftSide=document.createElement("article");
+	// leftSide.classList.add("leftSide");
+	// main.appendChild(leftSide);
+
+	// var image=document.createElement("section");
+	// image.classList.add("image");
+	// leftSide.appendChild(image);
+	// var img=document.createElement("img");
+	// img.src="image/avatar.svg";
+	// img.alt="Rajyalakshmi profile";
+
+	// image.appendChild(img);
+
+	// leftSide.appendChild(document.createElement("hr"));
+
+	// var profile=document.createElement("section");
+	// profile.classList.add("profile");
+	// leftSide.appendChild(profile);
+
+	// var name1=document.createElement("h2");
+	// name1.textContent="Rajyalakshmi";
+	// profile.appendChild(name1);
+
+	// var phone=document.createElement("h4");
+	// phone.textContent="+91 9704 122 768";
+	// profile.appendChild(phone);
+
+	// var email=document.createElement("h4");
+	// email.textContent="raajivvit@gmail.com";
+	// profile.appendChild(email);
+
+	// leftSide.appendChild(document.createElement("hr"));
+
+	// var address=document.createElement("section");
+	// address.classList.add("address");
+	// leftSide.appendChild(address);
+
+	// var gname1=document.createElement("p");
+	// gname1.textContent="W/O. Dr. T. K. Rao";
+	// address.appendChild(gname1);
+
+	// var flat=document.createElement("p");
+	// flat.textContent="F. No: 402, B Block";
+	// address.appendChild(flat);
+
+	// var street=document.createElement("p");
+	// street.textContent="Brindavan Apartment";
+	// address.appendChild(street);
+
+	// var city=document.createElement("p");
+	// city.textContent="Tenali";
+	// address.appendChild(city);
+
+	// var district=document.createElement("p");
+	// district.textContent="Guntur-522201";
+	// address.appendChild(district);
+
+
+
+
+		
